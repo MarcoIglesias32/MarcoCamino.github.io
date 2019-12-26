567fe6b070dde9e3fe0269d075bf312ff9b666b1
MarcoCamino777 <59253120+MarcoCamino777@users.noreply.github.com>
2019-12-26 21:00:24 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,9 @@ var rabbitEl = document.getElementById("conejo");
 
    var StartTime = new Date().getTime();
       var walkTheRabbit = function() {
-         var currTime
+         var currTime = new Date().getTime();
+         var secondsElapsed = ((currTime - startTime)/1000);
+         var newRight = (50 = ((currTime - startTime)/1000)*30);
+      };
+
+
