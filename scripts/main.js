b4a9216d4ff76f76a2dea88544439c603b6b67dd
MarcoCamino777 <59253120+MarcoCamino777@users.noreply.github.com>
2019-12-28 12:50:27 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,13 @@ var rabbitEl = document.getElementById("conejo");
          var currTime = new Date().getTime();
          var secondsElapsed = ((currTime - startTime)/1000);
          var newRight = (50 = ((currTime - startTime)/1000)*30);
+         
+      dogEl.style.right = newRight + "px";
+      window.requestAnimationFrame(walkTheRabbit)
       };
+
+walkTheRabbit();
+
+var button = document.getElementById("button");
 
 
